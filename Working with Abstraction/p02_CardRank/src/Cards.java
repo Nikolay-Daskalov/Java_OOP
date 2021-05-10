f98b1386209444eb9c8ef45c4637e4f7fad79a9b
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-10 12:29:55 +0300

    commit

---
+++ Working with Abstraction/p02_CardRank/src/Cards.java
@@ -0,0 +1,21 @@
+public enum Cards {
+    ACE("ACE"),
+    TWO("TWO"),
+    THREE("THREE"),
+    FOUR("FOUR"),
+    FIVE("FIVE"),
+    SIX("SIX"),
+    SEVEN("SEVEN"),
+    EIGHT("EIGHT"),
+    NINE("NINE"),
+    TEN("TEN"),
+    JACK("JACK"),
+    QUEEN("QUEEN"),
+    KING("KING");
+
+    private String name;
+
+    Cards(String name) {
+        this.name = name;
+    }
+}
