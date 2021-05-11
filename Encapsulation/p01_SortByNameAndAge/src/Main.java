6907f671c3bacd13dcbf863d101deb1423ebb37d
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-11 12:11:25 +0300

    commit

--- Encapsulation/p01_SortByNameAndAge/src/Main.java
+++ Encapsulation/p01_SortByNameAndAge/src/Main.java
@@ -3,11 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(reader.readLine());
 
         List<Person> people = new ArrayList<>();
@@ -16,16 +18,7 @@ public class Main {
             String[] input = reader.readLine().split(" ");
             people.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
         }
-
-        Collections.sort(people, (firstPerson, secondPerson) -> {
-            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
-
-            if (sComp != 0) {
-                return sComp;
-            } else {
-                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
-            }
-        });
+        Collections.sort(people, new PersonComparator());
 
         for (Person person : people) {
             System.out.println(person.toString());
