d827b1a99edbe2ebf6a833af76f46b65660320df
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-12 15:23:28 +0300

    commit

---
+++ Encapsulation/p02_SalaryIncrease/src/Person.java
@@ -0,0 +1,62 @@
+import java.text.DecimalFormat;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %s leva", this.firstName,
+                this.lastName, new DecimalFormat("#.0##").format(this.salary));
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.age < 30) {
+            bonus /= 2;
+            this.salary = this.getSalary() + this.getSalary() * (bonus / 100);
+        } else {
+            this.salary = this.getSalary() + this.getSalary() * (bonus / 100);
+        }
+    }
+}
