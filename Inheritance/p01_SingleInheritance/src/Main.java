4c0b19ddee17c4e550b49b7e1df9a3d239ef335a
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-13 12:50:34 +0300

    commit

--- Inheritance/p01_SingleInheritance/src/Main.java
+++ Inheritance/p01_SingleInheritance/src/Main.java
@@ -1,17 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) {
         Dog dog = new Dog();
         dog.eat();
         dog.bark();
-
-        reader.close();
     }
 }
