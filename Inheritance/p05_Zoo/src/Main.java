849cf0dbef2ebcca12de00af53d8c6195695279b
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-14 14:36:48 +0300

    Update Main.java

--- Inheritance/p05_Zoo/src/Main.java
+++ Inheritance/p05_Zoo/src/Main.java
@@ -8,7 +8,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        Animal snake = new Snake(reader.readLine());
+        Animal gorilla = new Gorilla(reader.readLine());
+        Animal mammal = new Mammal(reader.readLine());
 
+        System.out.println(snake.getName());
+        System.out.println(gorilla.getName());
+        System.out.println(mammal.getName());
 
         reader.close();
     }
