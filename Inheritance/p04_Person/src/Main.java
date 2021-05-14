1b4e7d190e9413672427aa3a0bae3592224127a7
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-14 14:31:39 +0300

    commit

---
+++ Inheritance/p04_Person/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        Child child = new Child(name, age);
+
+        System.out.println(child.getName());
+        System.out.println(child.getAge());
+
+        reader.close();
+    }
+}
