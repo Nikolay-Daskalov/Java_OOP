72bd2ecda4d1e641abb98fbe122bdea79f436a8c
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-15 16:28:10 +0300

    commit

---
+++ Interfaces and Abstraction/p02_CarShopExtended/src/Seat.java
@@ -0,0 +1,20 @@
+public class Seat extends CarImpl implements Sellable {
+    public Seat(String model, String color, int horsePower,
+                String countryProduced, double price) {
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    private Double price;
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s sells for %f",
+                super.toString(), this.getModel(), this.getPrice());
+    }
+}
