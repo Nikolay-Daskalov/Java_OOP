332c2c9be28456978b52ff56e6eaa69fe4fe55e6
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-15 16:22:57 +0300

    commit

---
+++ Interfaces and Abstraction/p01_CarShop/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Car seat = new Seat("Leon", "gray", 110, "Spain");
+
+        System.out.println(String.format(
+                "%s is %s color and have %s horse power",
+                seat.getModel(),
+                seat.getColor(),
+                seat.getHorsePower()));
+        System.out.println(seat.toString());
+
+
+        reader.close();
+    }
+}
