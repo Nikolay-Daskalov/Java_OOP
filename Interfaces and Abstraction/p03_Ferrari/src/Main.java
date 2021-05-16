49b00ac572efe080b7a28d6eeab21df83898f159
Kmetamazter <daskalovnikolaj@gmail.com>
2021-05-16 13:26:36 +0300

    commit

---
+++ Interfaces and Abstraction/p03_Ferrari/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Ferrari ferrari = new Ferrari(reader.readLine());
+
+        System.out.println(ferrari.toString());
+
+        reader.close();
+    }
+}
