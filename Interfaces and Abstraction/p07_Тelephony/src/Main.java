d87697abfb5861defb0df8e01805a0ac6803cb13
Niki <daskalovnikolaj@gmail.com>
2021-05-18 13:40:46 +0300

    Update Main.java

--- Interfaces and Abstraction/p07_Тelephony/src/Main.java
+++ Interfaces and Abstraction/p07_Тelephony/src/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
