279613927628d7113f20e74f0589e30cb1eaf169
Niki <daskalovnikolaj@gmail.com>
2021-05-18 13:48:18 +0300

    added files

---
+++ Polymorphism/p01_MathOperation/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        MathOperation math = new MathOperation();
+        System.out.println(math.add(2, 2));
+        System.out.println(math.add(3, 3, 3));
+        System.out.println(math.add(4, 4, 4, 4));
+
+        reader.close();
+    }
+}
