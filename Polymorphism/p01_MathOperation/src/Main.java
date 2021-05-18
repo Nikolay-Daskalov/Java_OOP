324056d61819d390f030000d91d28d363cf0aabc
Niki <daskalovnikolaj@gmail.com>
2021-05-18 13:49:13 +0300

    Update Main.java

--- Polymorphism/p01_MathOperation/src/Main.java
+++ Polymorphism/p01_MathOperation/src/Main.java
@@ -1,11 +1,6 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
