dbbb9d7e202d52d561aa391003a11a1624460763
Niki <daskalovnikolaj@gmail.com>
2021-05-20 15:05:19 +0300

    Update Main.java

--- Reflection/p01_Reflection/src/Main.java
+++ Reflection/p01_Reflection/src/Main.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
         Class<Reflection> reflection = Reflection.class;
         System.out.println(reflection.toString());
