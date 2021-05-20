561bd2e5b70e8f45615912c7d798168ee647efaa
Niki <daskalovnikolaj@gmail.com>
2021-05-20 14:50:19 +0300

    added files

---
+++ Reflection/p01_Reflection/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Class<Reflection> reflection = Reflection.class;
+        System.out.println(reflection.toString());
+        Class<? super Reflection> superclass = reflection.getSuperclass();
+        System.out.println(superclass);
+        Class<?>[] interfaces = reflection.getInterfaces();
+        for (Class<?> anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+        Reflection reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
