cf132f945e3e70e2570b3a1a229465a91f288c4d
Niki <daskalovnikolaj@gmail.com>
2021-05-20 14:56:54 +0300

    commit

--- Reflection/p01_Reflection/src/Main.java
+++ Reflection/p01_Reflection/src/Main.java
@@ -10,12 +10,12 @@ public class Main {
         Class<Reflection> reflection = Reflection.class;
         System.out.println(reflection.toString());
         Class<? super Reflection> superclass = reflection.getSuperclass();
-        System.out.println(superclass);
+        System.out.println(superclass.toString());
         Class<?>[] interfaces = reflection.getInterfaces();
         for (Class<?> anInterface : interfaces) {
-            System.out.println(anInterface);
+            System.out.println(anInterface.toString());
         }
         Reflection reflectionObject = reflection.getDeclaredConstructor().newInstance();
-        System.out.println(reflectionObject);
+        System.out.println(reflectionObject.toString());
     }
 }
