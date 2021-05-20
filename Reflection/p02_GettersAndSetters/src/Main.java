8c0bd5576aa16a149b64553e488cc699d5b9144e
Niki <daskalovnikolaj@gmail.com>
2021-05-20 15:00:15 +0300

    added files

---
+++ Reflection/p02_GettersAndSetters/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Method[] declaredMethods = Reflection.class.getDeclaredMethods();
+        Predicate<Method> predicate = method -> method.toString().contains("get");
+
+        Set<Method> getters = Arrays.stream(declaredMethods)
+                .filter(predicate)
+                .sorted(Comparator.comparing(Method::getName))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        predicate = method -> method.toString().contains("set");
+
+        Set<Method> setters = Arrays.stream(declaredMethods)
+                .filter(predicate)
+                .sorted(Comparator.comparing(Method::getName))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        for (Method getter : getters) {
+            System.out.printf("%s will return class %s",getter.getName(),getter.getReturnType().getName());
+            System.out.println();
+        }
+
+        for (Method setter : setters) {
+            System.out.printf("%s and will set field of class %s",setter.getName(),setter.getParameterTypes()[0].getName());
+            System.out.println();
+        }
+
+
+    }
+}
